52f4dab259fabbf15ebd0db0eec6e4815aab18d6
Katyayini Gupta <katyayinigupta@Katyayinis-MacBook-Air.local>
2026-09-21 21:30:06 +0530

    Remove node_modules and clean repository

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FntcRsNccUUS7hST2wvQCGlJmXcjTesBwZYGXXwhzac=\"\n}"
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1k2f3c0._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_0jm4bnn._.js","static/chunks/_04if9nj._.js"],
-    source: "entry"
-});
